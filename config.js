38960a840df23f003355441a8a6b76c754fe45d2
Glitch (encouraging-moored-gargoyleosaurus) <none>
2021-12-21 20:41:53 +0000

    🏐🐜 Checkpoint ./config.js:39921432/144

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ const config = {
   TOKEN: "", //Glitchde Kullanacak iseniz .env Dosyasına Tokeni Yazın!
 
   prefix: "!",
-  guild: "", //Sunucu ID Yazın
-  channel: "", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
+  guild: "919881098341810227", //Sunucu ID Yazın
+  channel: "922950932977823784", //Başvuru Yap Mesajının Atılacağı Kanal ID Yazın
 
   log: {
-    staff: "", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
-    channel: "" //Başvuruların Loglanacağı Kanal ID Yazın
+    staff: "922951340446064681", //Başvuru Onaylayıp-Reddedecek Yetkili Rol ID Yazın!
+    channel: "922950959288688673" //Başvuruların Loglanacağı Kanal ID Yazın
   },
 
   sonuc: {
-    staffRole: [""], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
-    sRole: "", //Verilecek En Düşük Yetkili Rol ID Yazın
+    staffRole: ["922951340446064681"], //Sunucunuzdaki Başlangıç Yetkili Rolü veya Rolleri, Birden Fazla Rol Eklemek İçin ["rol id 1", "rol id2"] Başvuru Onaylanınca Buraya Yazılan Rol veya Roller Kullanıcıya Verilecek
+    sRole: "919881167447130115", //Verilecek En Düşük Yetkili Rol ID Yazın
     successMessage: "Hey Tebrikler Yetkili Oldun!", //Başvuru Onaylanınca Kullanıcıya Atılacak Mesajı Yazın
     deleteMessage: "Üzgünüm Başvurun Reddedildi!", //Başvuru Reddedilince Kullanıcıya Atılacak Mesajı Yazın
     timeout: 20000 //Başvurusu Reddedilen Birisi Nekadar Süre Sonra Başvuru Yapa Bilsin? 1000=1 Saniye (Milisaniye Türünden Yazın)
